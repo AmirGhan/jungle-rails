e05aaeeec58e20181ee841a90eb286cfe2e4af42
Amir <amir.ghandeharioon@gmail.com>
2017-11-10 22:38:57 +0000

    review authentication

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_filter :authorize
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.create(review_params)
